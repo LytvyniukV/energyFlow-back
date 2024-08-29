299dd09f76536cac1e3adce2e75de49612eec0c5
LytvyniukV <lytvyniuk.vladyslav@gmail.com>
2024-08-29 20:17:07 +0200

    as

--- src/helpers/parsedFilters.js
+++ src/helpers/parsedFilters.js
@@ -2,7 +2,7 @@ export const parseFilter = (filter) => {
   const isString = typeof filter === 'string';
   if (!isString) return;
   const isFilter = (filter) =>
-    ['bodyparts', 'muscles', 'equipment'].includes(filter);
+    ['Body parts', 'Muscles', 'Equipment'].includes(filter);
 
   if (isFilter(filter)) return filter;
 };
