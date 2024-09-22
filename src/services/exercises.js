110edc7fbfc811950d7c2fe625fa23d31bd3b5fc
LytvyniukV <lytvyniuk.vladyslav@gmail.com>
2024-09-22 16:45:30 +0200

    upd

--- src/services/exercises.js
+++ src/services/exercises.js
@@ -76,7 +76,7 @@ const leaveReview = async (userId, exerciseId, userReview) => {
 
   if (!user || !exercise) throw HttpError(404);
   return {
-    review: exerciseReviews,
+    reviews: exerciseReviews,
     exercise,
   };
 };
